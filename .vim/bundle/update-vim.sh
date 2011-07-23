79d43a7af2dfd098b687b68be54777b425e7ace4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2011-07-24 04:14:40 +0900

    The script "update-vim.sh" supports Mercurial and Bazaar repository.

--- .vim/bundle/update-vim.sh
+++ .vim/bundle/update-vim.sh
@@ -3,6 +3,10 @@
 for d in ~/.vim/bundle/*; do
     if [ -d "$d/.git" ]; then
         (echo -ne "$d\t"; cd $d; git pull)
+    elif [ -d "$d/.hg" ]; then
+        (echo -ne "$d\t"; cd $d; hg pull --update)
+    elif [ -d "$d/.bzr" ]; then
+        (echo -ne "$d\t"; cd $d; bzr pull)
     elif [ -d "$d/.svn" ]; then
         (echo -ne "$d\t"; cd $d; svn update)
     fi
