2738b7f6f2767341dcd384a2256411a412eb1b73
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-01-06 01:59:12 +0900

    Separate user command definitions

--- init.lua
+++ init.lua
@@ -364,15 +364,7 @@ let g:neovide_cursor_vfx_mode = "railgun"
 
 
 " {{{ Commands
-command! ReloadVimrc source $MYVIMRC
-command! -nargs=? -bang Cp932 edit<bang> ++enc=cp932 <args>
-command! -nargs=? -bang Eucjp edit<bang> ++enc=euc-jp <args>
-command! -nargs=? -bang Sjis  edit<bang> ++enc=sjis <args>
-command! -nargs=? -bang Utf8  edit<bang> ++enc=utf-8 <args>
-command! -nargs=? -bang Utf16 edit<bang> ++enc=utf-16 <args>
-command! -nargs=? -bang Jis   edit<bang> ++enc=iso-2022-jp <args>
-command! -nargs=1 -complete=file Rename saveas <args> | call delete(expand('#'))
-command! Hitest source $VIMRUNTIME/syntax/hitest.vim
+lua require('commands')
 " }}}
 
 
