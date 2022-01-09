a307017ae49f615770fdfe888998b720902f7024
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-01-09 23:49:24 +0900

    Don't make a space visible

--- init.lua
+++ init.lua
@@ -691,8 +691,7 @@ let g:better_whitespace_filetypes_blacklist = [
 " {{{ lukas-reineke/indent-blankline.nvim
 lua <<EOF
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+vim.opt.listchars:append({ eol = '↴' })
 
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
