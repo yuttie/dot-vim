af60f7cdb7dedb06761b8085ce65a8c46df8f64b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-02-01 02:11:20 +0900

    Use shiftwidth=2 for lua

--- lua/filetype-autocmds.lua
+++ lua/filetype-autocmds.lua
@@ -4,6 +4,11 @@ autocmd MyAutoCmds FileType crystal setlocal shiftwidth=2
 " }}}
 
 
+" {{{ Lua
+autocmd MyAutoCmds FileType lua setlocal shiftwidth=2
+" }}}
+
+
 " {{{ TeX
 autocmd MyAutoCmds FileType tex setlocal shiftwidth=2
 autocmd MyAutoCmds FileType bib setlocal shiftwidth=2
