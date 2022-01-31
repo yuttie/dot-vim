8d2236c4d212963d0423da57003ed6bac3df7f11
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-02-01 01:03:12 +0900

    Enable rounded border

--- init.lua
+++ init.lua
@@ -1081,6 +1081,10 @@ end
 -- {{{ ray-x/lsp_signature.nvim
 require('lsp_signature').setup {
   hint_enable = false,
+  bind = true, -- This is mandatory, otherwise border config won't get registered.
+  handler_opts = {
+    border = "rounded",
+  },
 }
 -- }}}
 
