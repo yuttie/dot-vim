658b0c83fc7fb7bd8a387e369bd5926809e58ae2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-02-19 14:00:22 +0900

    Use Tree-sitter with indent-blankline.nvim

--- init.lua
+++ init.lua
@@ -632,6 +632,7 @@ require("indent_blankline").setup {
     show_current_context_start = true,
     show_end_of_line = true,
     space_char_blankline = " ",
+    use_treesitter = true,
 }
 -- }}}
 
