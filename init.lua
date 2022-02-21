48327b4b3bbdfbaa59ee89614b6c613531aeb2e7
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-02-22 03:05:08 +0900

    Fix indent

--- init.lua
+++ init.lua
@@ -512,14 +512,14 @@ autocmd MyAutoCmds BufNewFile * call InsertTemplate()
 
 -- {{{ nvim-web-devicons
 require'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- you can specify color or cterm_color instead of specifying both of them
- -- DevIcon will be appended to `name`
- override = {
- };
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
+  -- your personnal icons can go here (to override)
+  -- you can specify color or cterm_color instead of specifying both of them
+  -- DevIcon will be appended to `name`
+  override = {
+  };
+  -- globally enable default icons (default to false)
+  -- will get overriden by `get_icons` option
+  default = true;
 }
 -- }}}
 
