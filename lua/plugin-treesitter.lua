11785aef78ead5e7141c5f8f34b3c96d102e763d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-05-21 17:30:35 +0900

    Don't use twilight.nvim

--- lua/plugin-treesitter.lua
+++ lua/plugin-treesitter.lua
@@ -161,14 +161,6 @@ require('spellsitter').setup {
 }
 
 
--- folke/twilight.nvim
-require("twilight").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-}
-
-
 -- windwp/nvim-autopairs
 require('nvim-autopairs').setup {
 }
