adb32da14a932ee4ade815897e84ee05b14146d2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-10-22 02:46:49 +0900

    Update config to replace deprecated update_capabilities() with default_capabilities()

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -112,7 +112,7 @@ cmp.setup.filetype('lua', {
 do
   local lspconfig = require('lspconfig')
   local util = require('lspconfig/util')
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
   local on_attach = function(client, bufnr)
     -- Mappings.
