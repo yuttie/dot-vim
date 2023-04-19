5d5a8706086b464beeda4d53f194362f5e611802
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2023-04-20 00:34:57 +0900

    Configure kyazdani42/nvim-web-devicons only when it is loaded

--- init.lua
+++ init.lua
@@ -583,7 +583,22 @@ require('lazy').setup({
   --
   'nvim-lualine/lualine.nvim',
   { 'akinsho/bufferline.nvim', branch = 'dev' },
-  { 'kyazdani42/nvim-web-devicons', lazy = true },
+  {
+    'kyazdani42/nvim-web-devicons',
+    lazy = true,
+    config = function()
+      require'nvim-web-devicons'.setup {
+        -- your personnal icons can go here (to override)
+        -- you can specify color or cterm_color instead of specifying both of them
+        -- DevIcon will be appended to `name`
+        override = {
+        };
+        -- globally enable default icons (default to false)
+        -- will get overriden by `get_icons` option
+        default = true;
+      }
+    end,
+  },
   'folke/lsp-colors.nvim',
 })
 
@@ -747,20 +762,6 @@ endfunction
 autocmd MyAutoCmds BufNewFile * call InsertTemplate()
 " }}}
 ]=]
-
-
--- {{{ nvim-web-devicons
-require'nvim-web-devicons'.setup {
-  -- your personnal icons can go here (to override)
-  -- you can specify color or cterm_color instead of specifying both of them
-  -- DevIcon will be appended to `name`
-  override = {
-  };
-  -- globally enable default icons (default to false)
-  -- will get overriden by `get_icons` option
-  default = true;
-}
--- }}}
 
 
 -- {{{ norcalli/nvim-colorizer.lua
