68b880f781465eb6adaa6bb55795b8efdf3003b7
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2023-04-20 00:31:58 +0900

    Embed contents of lua/lsp.lua

--- init.lua
+++ init.lua
@@ -1353,7 +1353,21 @@ require('gitsigns').setup {
 -- }}}
 
 
-require('lsp')
+-- Configure diagnostic
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+-- Diagnostic symbols in the sign column
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 
 -- {{{ rhysd/vim-grammarous
