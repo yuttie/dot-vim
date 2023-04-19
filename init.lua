1363da1e08b98abde81f972e6ab347b1178303bb
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2023-04-20 01:21:26 +0900

    Configure telescope.nvim on demand

--- init.lua
+++ init.lua
@@ -439,6 +439,29 @@ require('lazy').setup({
       'nvim-telescope/telescope-file-browser.nvim',
       'nvim-telescope/telescope-ui-select.nvim',
     },
+    cmd = {
+      'Telescope',
+    },
+    config = function()
+      require('telescope').setup{
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                -- map actions.which_key to <C-h> (default: <C-/>)
+                -- actions.which_key shows the mappings for your picker,
+                -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                ["<C-d>"] = "delete_buffer",
+              },
+            },
+          },
+        },
+      }
+      -- To get fzf loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require('telescope').load_extension('fzf')
+      require('telescope').load_extension('ui-select')
+    end,
   },
 
   --
@@ -1148,28 +1171,6 @@ vim.api.nvim_set_keymap('', '<ScrollWheelUp>',   ':call comfortable_motion#flick
 -- {{{ vim-expand-region
 vim.api.nvim_set_keymap('v', '+', '<Plug>(expand_region_expand)', {})
 vim.api.nvim_set_keymap('v', '-', '<Plug>(expand_region_shrink)', {})
--- }}}
-
-
--- {{{ nvim-telescope/telescope.nvim
-require('telescope').setup{
-  pickers = {
-    buffers = {
-      mappings = {
-        i = {
-          -- map actions.which_key to <C-h> (default: <C-/>)
-          -- actions.which_key shows the mappings for your picker,
-          -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-          ["<C-d>"] = "delete_buffer",
-        },
-      },
-    },
-  },
-}
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('ui-select')
 -- }}}
 
 
