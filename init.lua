7871ef728b332efebfe99a1cc9c779bcf9802444
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2023-05-13 12:39:17 +0900

    Workaround for <CR> with cmp-nvim-lsp-signature-help enabled

--- init.lua
+++ init.lua
@@ -515,7 +515,23 @@ require('lazy').setup({
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           -- ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- Workaround for cmp-nvim-lsp-signature-help
+          -- Borrowed from https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13#issuecomment-1157496673
+          ['<CR>'] = function(fallback)
+            -- Don't block <CR> if signature help is active
+            -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13
+            if not cmp.visible() or not cmp.get_selected_entry() or cmp.get_selected_entry().source.name == 'nvim_lsp_signature_help' then
+              fallback()
+            else
+              cmp.confirm({
+                -- Replace word if completing in the middle of a word
+                -- https://github.com/hrsh7th/nvim-cmp/issues/664
+                behavior = cmp.ConfirmBehavior.Replace,
+                -- Don't select first item on CR if nothing was selected
+                select = false,
+              })
+            end
+          end
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
