23218330199e10299d91ec9631d25a217fe12334
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2023-10-19 20:48:01 +0900

    nvim-cmp: Disable autocomplete for search

--- init.lua
+++ init.lua
@@ -566,14 +566,6 @@ require('lazy').setup({
         },
       })
 
-      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
