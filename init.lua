366f43f3c3c5958dc3dcbfafe2b81a8ffe91a247
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-05-31 02:41:59 +0900

    Let vim-matchup ignore middle words, e.g. `return` for highlight and motion

--- init.lua
+++ init.lua
@@ -894,6 +894,9 @@ require('lazy').setup({
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
     },
+    config = function()
+      vim.g.matchup_delim_nomids = 1
+    end,
   },
   {
     'windwp/nvim-ts-autotag',
