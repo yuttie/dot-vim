11ac064e60b2a49caee1c0a5f13121b8811da746
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-07-12 19:35:54 +0900

    Disable lsp_lines.nvim in lazy.nvim's window
    Borrowed from https://www.reddit.com/r/neovim/comments/125w37h/comment/jecdme4/

--- init.lua
+++ init.lua
@@ -616,6 +616,17 @@ require('lazy').setup({
     'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
     config = function()
       require('lsp_lines').setup()
+
+      -- Disable it in lazy.nvim's window
+      vim.api.nvim_create_autocmd('WinEnter', {
+        callback = function()
+          local floating = vim.api.nvim_win_get_config(0).relative ~= ""
+          vim.diagnostic.config({
+            virtual_text = floating,
+            virtual_lines = not floating,
+          })
+        end,
+      })
     end,
   },
   -- Status
