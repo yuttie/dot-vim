8cd09c73a91b99ab678932c86f9165e31efa66bd
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-09-10 19:33:59 +0900

    Lazy load nvim-treesitter-context on BufRead event

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -150,6 +150,7 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
+    event = 'BufRead',
     config = function()
       require('treesitter-context').setup {
         enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
