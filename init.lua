002f764ed60903955039e365d64dd885f10d9757
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-09-10 17:19:38 +0900

    Move config for lualine.nvim from init.lua to lua/plugins/ui.lua

--- init.lua
+++ init.lua
@@ -229,39 +229,6 @@ require'colorizer'.setup({
 -- }}}
 
 
--- {{{ nvim-lualine/lualine.nvim
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'filename'},
-    lualine_c = {'branch', 'diff', 'diagnostics'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'filename'},
-    lualine_c = {'branch', 'diff', 'diagnostics'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  tabline = {
-  },
-  extensions = {}
-}
--- }}}
-
-
 -- {{{ akinsho/bufferline.nvim
 require('bufferline').setup {
   options = {
