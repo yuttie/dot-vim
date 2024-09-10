2317a53c1cafd4a91914e92b54514de5e8968b5d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-09-10 19:34:46 +0900

    Lazy load nvim-treesitter on BufRead event

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    event = 'BufRead',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-refactor',
       'nvim-treesitter/nvim-treesitter-textobjects',
