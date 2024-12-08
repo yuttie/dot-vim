0692283aded3476209eec1379687147ffbb7fc31
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-09 01:20:17 +0900

    feat: Add plugin 'folke/noice.nvim'

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -226,6 +226,38 @@ return {
   --   end,
   -- },
   {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          -- override the default lsp markdown formatter with Noice
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          -- override the lsp markdown formatter with Noice
+          ['vim.lsp.util.stylize_markdown'] = true,
+          -- override cmp documentation with Noice (needs the other options to work)
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      'folke/snacks.nvim',
+      -- 'rcarriga/nvim-notify',
+    },
+  },
+  {
     'sindrets/winshift.nvim',
     config = function()
       require("winshift").setup {
