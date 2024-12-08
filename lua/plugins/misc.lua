ad639321f3f24c16b529fe98fd1ebdcc951b9056
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-08 19:06:18 +0900

    Install 'folke/snacks.nvim'

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,4 +5,16 @@ return {
       '<Plug>(openbrowser-smart-search)',
     },
   },
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
 }
