657c46df06b1c8a415e1b527c1d4de774c470909
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-11 11:51:24 +0900

    fix: Disable workaround for disabling virtual_lines in lazy.nvim's floating window
    The implementation of the workaround is targeting too wide kinds of windows
    including normal windows, and therefore settings for normal windows are also
    modified, whichi is not what I want.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -342,15 +342,15 @@ return {
       })
 
       -- Disable it in lazy.nvim's window
-      vim.api.nvim_create_autocmd('WinEnter', {
-        callback = function()
-          local floating = vim.api.nvim_win_get_config(0).relative ~= ""
-          vim.diagnostic.config({
-            virtual_text = floating,
-            virtual_lines = not floating,
-          })
-        end,
-      })
+      -- vim.api.nvim_create_autocmd('WinEnter', {
+      --   callback = function()
+      --     local floating = vim.api.nvim_win_get_config(0).relative ~= ""
+      --     vim.diagnostic.config({
+      --       virtual_text = floating,
+      --       virtual_lines = not floating,
+      --     })
+      --   end,
+      -- })
     end,
   },
   -- Navigation
