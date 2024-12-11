da17198172b765a750f7f4a7a5c8b379bdc85b5c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-11 22:08:25 +0900

    feat(lsp_lines.nvim): Enable only for error diagnostics

--- init.lua
+++ init.lua
@@ -389,6 +389,7 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+require('filter-diagnostics').set_level('virtual_lines', vim.diagnostic.severity.ERROR)
 
 
 -- {{{ rhysd/vim-grammarous
