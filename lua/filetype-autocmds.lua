af0a8bf4c08df1cbb1f6f32f1e85561732f1b7bc
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-12 13:31:07 +0900

    feat: Set shiftwidth to my preferred value for all file types
    Removed filetype-specific autocmds to use my preferred shiftwidth value across all files.
    This change prioritizes readability based on my personal coding style.
    Adjustments can still be made on a per-project basis if needed.

--- lua/filetype-autocmds.lua
+++ lua/filetype-autocmds.lua
@@ -1,12 +1,6 @@
 vim.cmd [=[
-" {{{ Crystal
-autocmd MyAutoCmds FileType crystal setlocal shiftwidth=2  " Rationale: Official coding style guide specifies 2 spaces
-" }}}
-
-
 " {{{ fish
 autocmd MyAutoCmds FileType fish setlocal foldmethod=expr
-autocmd MyAutoCmds FileType fish setlocal shiftwidth=4  " Rationale: `fish_indent` uses 4 spaces
 " }}}
 
 
@@ -18,19 +12,7 @@ autocmd MyAutoCmds FileType help setl conceallevel=0
 
 
 " {{{ TeX
-autocmd MyAutoCmds FileType tex setlocal shiftwidth=2
-autocmd MyAutoCmds FileType bib setlocal shiftwidth=2
 autocmd MyAutoCmds FileType tex setlocal wrap
-" }}}
-
-
-" {{{ Shell Scripts
-autocmd MyAutoCmds FileType sh,zsh setlocal shiftwidth=2
-" }}}
-
-
-" {{{ Ruby
-autocmd MyAutoCmds FileType ruby,eruby setlocal shiftwidth=2
 " }}}
 
 
@@ -38,32 +20,10 @@ autocmd MyAutoCmds FileType ruby,eruby setlocal shiftwidth=2
 autocmd MyAutoCmds FileType python setlocal tabstop=8
 autocmd MyAutoCmds FileType python setlocal expandtab
 autocmd MyAutoCmds FileType python setlocal softtabstop=4
-autocmd MyAutoCmds FileType python setlocal shiftwidth=4  " Rationale: PEP 8: Use 4 spaces per indentation level.
-" }}}
-
-
-" {{{ Elm
-autocmd MyAutoCmds FileType elm setlocal shiftwidth=2  " Rationale: Official documentation uses 2 spaces
 " }}}
 
 
 " {{{ Vim
 autocmd MyAutoCmds FileType vim setlocal keywordprg=:help
-autocmd MyAutoCmds FileType vim setlocal shiftwidth=2
-" }}}
-
-
-" {{{ Web
-autocmd MyAutoCmds FileType javascript setlocal shiftwidth=2
-autocmd MyAutoCmds FileType typescript setlocal shiftwidth=2
-autocmd MyAutoCmds FileType html       setlocal shiftwidth=2
-autocmd MyAutoCmds FileType css        setlocal shiftwidth=2
-autocmd MyAutoCmds FileType scss       setlocal shiftwidth=2
-autocmd MyAutoCmds FileType vue        setlocal shiftwidth=2
-" }}}
-
-
-" {{{ XML
-autocmd MyAutoCmds FileType xml setlocal shiftwidth=2
 " }}}
 ]=]
