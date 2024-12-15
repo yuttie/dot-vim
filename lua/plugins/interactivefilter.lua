bdeda35ebdae5bcadc59452e613257c9df04f37e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-15 20:18:22 +0900

    docs: Clean up

--- lua/plugins/interactivefilter.lua
+++ lua/plugins/interactivefilter.lua
@@ -27,18 +27,33 @@ return {
     },
     config = function()
       require('telescope').setup{
+        defaults = {
+          -- Default configuration for telescope goes here:
+          -- config_key = value,
+        },
         pickers = {
+          -- Default configuration for builtin pickers goes here:
+          -- picker_name = {
+          --   picker_config_key = value,
+          --   ...
+          -- }
+          -- Now the picker_config_key will be applied every time you call this
+          -- builtin picker
           buffers = {
             mappings = {
               i = {
-                -- map actions.which_key to <C-h> (default: <C-/>)
-                -- actions.which_key shows the mappings for your picker,
-                -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                ["<C-d>"] = "delete_buffer",
+                ['<C-d>'] = 'delete_buffer',
               },
             },
           },
         },
+        extensions = {
+          -- Your extension configuration goes here:
+          -- extension_name = {
+          --   extension_config_key = value,
+          -- }
+          -- please take a look at the readme of the extension you want to configure
+        }
       }
       -- To get fzf loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
