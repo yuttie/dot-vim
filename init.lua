4d57e5817d4d62693b7893ee2f884154085e9f38
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-15 16:44:19 +0900

    feat: Let lazy.nvim notify package updates

--- init.lua
+++ init.lua
@@ -48,7 +48,10 @@ require('lazy').setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = true,
+  },
 })
 
 -- }}}
