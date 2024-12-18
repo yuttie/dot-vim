7766cb28a03c13adebc993ba7ce988483c24efe2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-19 00:54:49 +0900

    Revert commit 'ea31597a' because this does not work for the purpose

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -280,12 +280,7 @@ return {
                     end,
                 }),
                 sources = cmp.config.sources({
-                    {
-                        name = "nvim_lsp",
-                        entry_filter = function(entry, ctx)
-                            return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
-                        end,
-                    },
+                    { name = "nvim_lsp" },
                     { name = "nvim_lsp_signature_help" },
                     { name = "vsnip" }, -- For vsnip users.
                     { name = "spell" },
