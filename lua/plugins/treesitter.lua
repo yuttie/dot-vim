bac4a0026432dd558fff4fcd27984cb5890aa606
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-12-29 22:35:22 +0900

    feat: Adjust keymaps for incremental_selection tree-sitter module

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -38,9 +38,8 @@ return {
                     enable = true,
                     keymaps = {
                         init_selection = "gnn",
-                        node_incremental = "grn",
-                        scope_incremental = "grc",
-                        node_decremental = "grm",
+                        node_incremental = "gr+",
+                        node_decremental = "gr-",
                     },
                 },
 
