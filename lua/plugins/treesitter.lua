42f87ce70d434de953820c5e156f62e4cc9f9769
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-11-23 05:30:59 +0900

    fix(performance): disable treesitter entirely
    Neovim had noticeable lag when holding down j/k for continuous scrolling, even
    when running locally.
    
    Disabling Treesitter entirely removes the lag, restoring smooth scrolling
    performance. This commit comments out the configuration to stop loading
    Treesitter until a better workaround or optimization is found.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,210 +1,210 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
-        lazy = false,
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-refactor",
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            "andymass/vim-matchup",
-            "windwp/nvim-ts-autotag",
-        },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-                ensure_installed = "all",
-
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
-
-                -- List of parsers to ignore installing (or "all")
-                ignore_install = {},
-
-                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-                highlight = {
-                    enable = true,
-
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                },
-
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "gnn",
-                        node_incremental = "gr+",
-                        node_decremental = "gr-",
-                    },
-                },
-
-                indent = {
-                    enable = true,
-                    disable = {},
-                },
-
-                refactor = {
-                    highlight_definitions = {
-                        enable = true,
-                        clear_on_cursor_move = false,
-                    },
-                    highlight_current_scope = { enable = false },
-                    smart_rename = {
-                        enable = true,
-                        keymaps = {
-                            smart_rename = "gR",
-                        },
-                    },
-                    navigation = {
-                        enable = true,
-                        keymaps = {
-                            goto_definition_lsp_fallback = "gnd",
-                            list_definitions = "gnD",
-                            list_definitions_toc = "gO",
-                            goto_next_usage = "<a-*>",
-                            goto_previous_usage = "<a-#>",
-                        },
-                    },
-                },
-
-                textobjects = {
-                    select = {
-                        enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["g>"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["g<"] = "@parameter.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                    lsp_interop = {
-                        enable = true,
-                        border = "none",
-                        peek_definition_code = {
-                            ["<leader>df"] = "@function.outer",
-                            ["<leader>dF"] = "@class.outer",
-                        },
-                    },
-                },
-
-                matchup = {
-                    enable = true, -- mandatory, false will disable the whole extension
-                    disable = {}, -- optional, list of language that will be disabled
-                    -- [options]
-                },
-
-                autotag = {
-                    enable = true,
-                },
-            })
-        end,
-    },
-    {
-        "nvim-treesitter/playground",
-        cmd = {
-            "TSPlaygroundToggle",
-            "TSHighlightCapturesUnderCursor",
-            "TSNodeUnderCursor",
-        },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        event = "BufRead",
-        config = function()
-            require("treesitter-context").setup({
-                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-                min_window_height = 15,
-                trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-
-                -- [!] The options below are exposed but shouldn't require your attention,
-                --     you can safely ignore them.
-
-                zindex = 20, -- The Z-index of the context window
-                mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-                -- Separator between context and content. Should be a single character string, like '-'.
-                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-                separator = nil,
-            })
-        end,
-    },
-    { "HiPhish/rainbow-delimiters.nvim" },
-    {
-        "andymass/vim-matchup",
-        config = function()
-            vim.g.matchup_delim_nomids = 1
-        end,
-    },
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup({})
-        end,
-    },
-    {
-        "mfussenegger/nvim-treehopper",
-        keys = {
-            { "m", ":<C-U>lua require('tsht').nodes()<CR>", mode = "o", silent = true },
-            { "m", ":lua require('tsht').nodes()<CR>", mode = "v", silent = true },
-        },
-    },
-    {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        config = function()
-            vim.g.skip_ts_context_commentstring_module = true
-            require("ts_context_commentstring").setup({})
-        end,
-    },
+    -- {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     branch = 'master',
+    --     lazy = false,
+    --     build = ":TSUpdate",
+    --     dependencies = {
+    --         "nvim-treesitter/nvim-treesitter-refactor",
+    --         "nvim-treesitter/nvim-treesitter-textobjects",
+    --         "andymass/vim-matchup",
+    --         "windwp/nvim-ts-autotag",
+    --     },
+    --     config = function()
+    --         require("nvim-treesitter.configs").setup({
+    --             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    --             ensure_installed = "all",
+    --
+    --             -- Install parsers synchronously (only applied to `ensure_installed`)
+    --             sync_install = false,
+    --
+    --             -- Automatically install missing parsers when entering buffer
+    --             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    --             auto_install = true,
+    --
+    --             -- List of parsers to ignore installing (or "all")
+    --             ignore_install = {},
+    --
+    --             ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    --             -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    --
+    --             highlight = {
+    --                 enable = true,
+    --
+    --                 disable = function(lang, buf)
+    --                     local max_filesize = 100 * 1024 -- 100 KB
+    --                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    --                     if ok and stats and stats.size > max_filesize then
+    --                         return true
+    --                     end
+    --                 end,
+    --
+    --                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    --                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    --                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    --                 -- Instead of true it can also be a list of languages
+    --                 additional_vim_regex_highlighting = false,
+    --             },
+    --
+    --             incremental_selection = {
+    --                 enable = true,
+    --                 keymaps = {
+    --                     init_selection = "gnn",
+    --                     node_incremental = "gr+",
+    --                     node_decremental = "gr-",
+    --                 },
+    --             },
+    --
+    --             indent = {
+    --                 enable = true,
+    --                 disable = {},
+    --             },
+    --
+    --             refactor = {
+    --                 highlight_definitions = {
+    --                     enable = true,
+    --                     clear_on_cursor_move = false,
+    --                 },
+    --                 highlight_current_scope = { enable = false },
+    --                 smart_rename = {
+    --                     enable = true,
+    --                     keymaps = {
+    --                         smart_rename = "gR",
+    --                     },
+    --                 },
+    --                 navigation = {
+    --                     enable = true,
+    --                     keymaps = {
+    --                         goto_definition_lsp_fallback = "gnd",
+    --                         list_definitions = "gnD",
+    --                         list_definitions_toc = "gO",
+    --                         goto_next_usage = "<a-*>",
+    --                         goto_previous_usage = "<a-#>",
+    --                     },
+    --                 },
+    --             },
+    --
+    --             textobjects = {
+    --                 select = {
+    --                     enable = true,
+    --
+    --                     -- Automatically jump forward to textobj, similar to targets.vim
+    --                     lookahead = true,
+    --
+    --                     keymaps = {
+    --                         -- You can use the capture groups defined in textobjects.scm
+    --                         ["af"] = "@function.outer",
+    --                         ["if"] = "@function.inner",
+    --                         ["ac"] = "@class.outer",
+    --                         ["ic"] = "@class.inner",
+    --                     },
+    --                 },
+    --                 swap = {
+    --                     enable = true,
+    --                     swap_next = {
+    --                         ["g>"] = "@parameter.inner",
+    --                     },
+    --                     swap_previous = {
+    --                         ["g<"] = "@parameter.inner",
+    --                     },
+    --                 },
+    --                 move = {
+    --                     enable = true,
+    --                     set_jumps = true, -- whether to set jumps in the jumplist
+    --                     goto_next_start = {
+    --                         ["]m"] = "@function.outer",
+    --                         ["]]"] = "@class.outer",
+    --                     },
+    --                     goto_next_end = {
+    --                         ["]M"] = "@function.outer",
+    --                         ["]["] = "@class.outer",
+    --                     },
+    --                     goto_previous_start = {
+    --                         ["[m"] = "@function.outer",
+    --                         ["[["] = "@class.outer",
+    --                     },
+    --                     goto_previous_end = {
+    --                         ["[M"] = "@function.outer",
+    --                         ["[]"] = "@class.outer",
+    --                     },
+    --                 },
+    --                 lsp_interop = {
+    --                     enable = true,
+    --                     border = "none",
+    --                     peek_definition_code = {
+    --                         ["<leader>df"] = "@function.outer",
+    --                         ["<leader>dF"] = "@class.outer",
+    --                     },
+    --                 },
+    --             },
+    --
+    --             matchup = {
+    --                 enable = true, -- mandatory, false will disable the whole extension
+    --                 disable = {}, -- optional, list of language that will be disabled
+    --                 -- [options]
+    --             },
+    --
+    --             autotag = {
+    --                 enable = true,
+    --             },
+    --         })
+    --     end,
+    -- },
+    -- {
+    --     "nvim-treesitter/playground",
+    --     cmd = {
+    --         "TSPlaygroundToggle",
+    --         "TSHighlightCapturesUnderCursor",
+    --         "TSNodeUnderCursor",
+    --     },
+    -- },
+    -- {
+    --     "nvim-treesitter/nvim-treesitter-context",
+    --     event = "BufRead",
+    --     config = function()
+    --         require("treesitter-context").setup({
+    --             enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    --             max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    --             min_window_height = 15,
+    --             trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    --
+    --             -- [!] The options below are exposed but shouldn't require your attention,
+    --             --     you can safely ignore them.
+    --
+    --             zindex = 20, -- The Z-index of the context window
+    --             mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+    --             -- Separator between context and content. Should be a single character string, like '-'.
+    --             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    --             separator = nil,
+    --         })
+    --     end,
+    -- },
+    -- { "HiPhish/rainbow-delimiters.nvim" },
+    -- {
+    --     "andymass/vim-matchup",
+    --     config = function()
+    --         vim.g.matchup_delim_nomids = 1
+    --     end,
+    -- },
+    -- {
+    --     "windwp/nvim-autopairs",
+    --     event = "InsertEnter",
+    --     config = function()
+    --         require("nvim-autopairs").setup({})
+    --     end,
+    -- },
+    -- {
+    --     "mfussenegger/nvim-treehopper",
+    --     keys = {
+    --         { "m", ":<C-U>lua require('tsht').nodes()<CR>", mode = "o", silent = true },
+    --         { "m", ":lua require('tsht').nodes()<CR>", mode = "v", silent = true },
+    --     },
+    -- },
+    -- {
+    --     "JoosepAlviste/nvim-ts-context-commentstring",
+    --     config = function()
+    --         vim.g.skip_ts_context_commentstring_module = true
+    --         require("ts_context_commentstring").setup({})
+    --     end,
+    -- },
 }
