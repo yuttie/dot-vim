4c5b7dae8b7bde41d383da373268e032a642a443
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-11-23 05:14:45 +0900

    chore(treesitter): update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,21 +12,32 @@ return {
         },
         config = function()
             require("nvim-treesitter.configs").setup({
-                -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                 ensure_installed = "all",
 
-                -- Install languages synchronously (only applied to `ensure_installed`)
+                -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
 
-                -- List of parsers to ignore installing
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                -- List of parsers to ignore installing (or "all")
                 ignore_install = {},
 
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
                 highlight = {
-                    -- `false` will disable the whole extension
                     enable = true,
 
-                    -- list of language that will be disabled
-                    disable = {},
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
 
                     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -50,7 +61,10 @@ return {
                 },
 
                 refactor = {
-                    highlight_definitions = { enable = true },
+                    highlight_definitions = {
+                        enable = true,
+                        clear_on_cursor_move = false,
+                    },
                     highlight_current_scope = { enable = false },
                     smart_rename = {
                         enable = true,
