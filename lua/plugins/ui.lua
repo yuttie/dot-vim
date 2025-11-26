26eca7dcb60b5c95e331d206e5a0950ed27e32e0
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-11-27 01:58:32 +0900

    refactor(plugins): follow repository name change

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -569,19 +569,8 @@ return {
     },
     "akinsho/bufferline.nvim",
     {
-        "kyazdani42/nvim-web-devicons",
-        lazy = true,
-        config = function()
-            require("nvim-web-devicons").setup({
-                -- your personnal icons can go here (to override)
-                -- you can specify color or cterm_color instead of specifying both of them
-                -- DevIcon will be appended to `name`
-                override = {},
-                -- globally enable default icons (default to false)
-                -- will get overriden by `get_icons` option
-                default = true,
-            })
-        end,
+        "nvim-tree/nvim-web-devicons",
+        opts = {},
     },
     {
         "sphamba/smear-cursor.nvim",
