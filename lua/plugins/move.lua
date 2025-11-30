86e02826a12905bf3d9b9943fab855058cba08e0
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-11-30 23:41:07 +0900

    chore(flash.nvim): disable `f`, `F`, `t`, `T`, `;` and `,` motions

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -5,6 +5,33 @@ return {
         ---@type Flash.Config
         opts = {
             labels = "aoeidtn',.pyfgcrl;qjkxbmuh",  -- Dvorak
+            highlight = {
+                -- show a backdrop with hl FlashBackdrop
+                backdrop = true,
+                -- Highlight the search matches
+                matches = true,
+                -- extmark priority
+                priority = 5000,
+                groups = {
+                    match = "FlashMatch",
+                    current = "FlashCurrent",
+                    backdrop = "FlashBackdrop",
+                    label = "FlashLabel",
+                },
+            },
+            ---@type table<string, Flash.Config>
+            modes = {
+                -- options used when flash is activated through
+                -- a regular search with `/` or `?`
+                search = {
+                    enabled = false,
+                },
+                -- options used when flash is activated through
+                -- `f`, `F`, `t`, `T`, `;` and `,` motions
+                char = {
+                    enabled = false,
+                },
+            },
         },
         keys = {
             { "<Space><Space>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
