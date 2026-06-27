c4ed3973d0c3536e2779c5b198188bc90da9e5c2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-06-27 21:26:03 +0900

    refactor: extract template insertion logic into lua/template_insertion.lua

--- init.lua
+++ init.lua
@@ -72,40 +72,7 @@ require("key_mapping")
 
 
 -- {{{ Template insertion
-vim.cmd([=[
-let template_dir = "~/.config/nvim/template"
-function! InsertTemplate()
-  if expand("%:t") == "NOTE"
-    let tmpl_filename = expand(g:template_dir) . "/NOTE"
-    if filereadable(tmpl_filename)
-      execute "silent 0read " . tmpl_filename
-
-      " delete trailing line
-      mark s
-      $delete
-      's
-    endif
-  else
-    let tmpl_filename = expand(g:template_dir) . "/tmpl." . expand("%:e")
-    if filereadable(tmpl_filename)
-      execute "silent 0read " . tmpl_filename
-
-      call FillTemplatePlaceHolders()
-
-      " delete trailing line
-      mark s
-      $delete
-      's
-    endif
-  endif
-endfunction
-
-function! FillTemplatePlaceHolders()
-  silent! %s/%HEADERNAME%/\=toupper(tr(expand("%:t"), ".", "_"))/g
-endfunction
-
-autocmd MyAutoCmds BufNewFile * call InsertTemplate()
-]=])
+require("template_insertion")
 -- }}}
 
 -- {{{ norcalli/nvim-colorizer.lua
